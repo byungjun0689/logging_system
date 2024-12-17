20575e5c058fa6d373cbf5a47201ad02b0b34c91
barnacles.lee <barnacles.lee@kakaohealthcare.com>
2024-12-17 14:34:28 +0900

    change spark log

--- yarn_spark/fluent-bit/filter_yarn_log.lua
+++ yarn_spark/fluent-bit/filter_yarn_log.lua
@@ -7,36 +7,36 @@ function filter_khc_yarn_log(tag, timestamp, record)
         record["applicationid"] = applicationid
     end
     
-    local message = record["log"]
+    local log_message = record["log"]
 
-    -- Step 정보 추출 (예시)
     -- 로그 메시지 필터링
-    if string.match(message, "^%d%d/%d%d/%d%d %d%d:%d%d:%d%d INFO KHCLogger: Step %d+: %d+:%d+:%d+") then
+    --if string.match(message, "^%d%d/%d%d/%d%d %d%d:%d%d:%d%d INFO KHCLogger: Step %d+: %d+:%d+:%d+") then
+    log_pattern = "^(%d%d/%d%d/%d%d)%s(%d%d:%d%d:%d%d)%s(%u+)%s(.+)"
+    local date, time, level, message = string.match(log_message, log_pattern)
 
-        local date, time, level = string.match(message, "(%d%d/%d%d/%d%d) (%d%d:%d%d:%d%d) (%u+)")
-        local day, month, year = string.match(date, "(%d%d)/(%d%d)/(%d%d)")
+    local log_data = {
+        date = date,
+        time = time,
+        level = level,
+        message = message
+    }
+
+    if date and time and level and message then
+        
+        local day, month, year = string.match(log_data.date, "(%d%d)/(%d%d)/(%d%d)")
+        local formatted_date = string.format("20%s-%s-%s", year, month, day)
         local formatted_date = string.format("20%s-%s-%s", year, month, day)
 
         record["log_date"] = formatted_date
-        record["log_time"] = time
-        record["log_level"] = level
-        record["log_datetime"] = formatted_date .. " " .. time
-
-        return 1, timestamp, record
-    elseif string.match(message, "^%d%d/%d%d/%d%d %d%d:%d%d:%d%d ERROR") then
-
-        local date, time, level = string.match(message, "(%d%d/%d%d/%d%d) (%d%d:%d%d:%d%d) (%u+)")
-        local day, month, year = string.match(date, "(%d%d)/(%d%d)/(%d%d)")
-        local formatted_date = string.format("20%s-%s-%s", year, month, day)
-
-        record["log_date"] = formatted_date
-        record["log_time"] = time
-        record["log_datetime"] = formatted_date .. " " .. time
-        record["log_level"] = level
+        record["log_time"] = log_data.time
+        record["level_info"] = log_data.level
+        record["log_datetime"] = formatted_date .. " " .. log_data.time
+        record["message"] = log_data.message
+        
+        record["log"] = nil
 
         return 1, timestamp, record
     else
         return -1, timestamp, record
     end
-
 end
