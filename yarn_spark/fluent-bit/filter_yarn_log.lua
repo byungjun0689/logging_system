bb611673976bb24fdec6abe3998a7f4181402d1d
barnacles.lee <barnacles.lee@kakaohealthcare.com>
2025-04-11 15:44:32 +0900

    fluent-bit 관련 conf uplaod

--- yarn_spark/fluent-bit/filter_yarn_log.lua
+++ yarn_spark/fluent-bit/filter_yarn_log.lua
@@ -1,17 +1,17 @@
 function filter_khc_yarn_log(tag, timestamp, record)
-    
     local path = record["file_path"]
     local applicationid = string.match(path, "application_%d+_%d+")
     
-    if applicationid then
-        record["applicationid"] = applicationid
-    end
     
+    if applicationid then
+        record["application_id"] = applicationid
+    end
+
     local log_message = record["log"]
 
     -- 로그 메시지 필터링
-    --if string.match(message, "^%d%d/%d%d/%d%d %d%d:%d%d:%d%d INFO KHCLogger: Step %d+: %d+:%d+:%d+") then
     log_pattern = "^(%d%d/%d%d/%d%d)%s(%d%d:%d%d:%d%d)%s(%u+)%s(.+)"
+    
     local date, time, level, message = string.match(log_message, log_pattern)
 
     local log_data = {
@@ -21,22 +21,39 @@ function filter_khc_yarn_log(tag, timestamp, record)
         message = message
     }
 
+    -- Get current date and time
+    local current_date = os.date("%Y-%m-%d")
+    local current_time = os.date("%H:%M:%S")
+    local current_datetime = current_date .. " " .. current_time
+
+    
+    record["log_datetime"] = current_datetime   
+    record["log_date"] = current_date
+    record["log_time"] = current_time
+
     if date and time and level and message then
         
-        local day, month, year = string.match(log_data.date, "(%d%d)/(%d%d)/(%d%d)")
-        local formatted_date = string.format("20%s-%s-%s", year, month, day)
-        local formatted_date = string.format("20%s-%s-%s", year, month, day)
+        -- local year, month, day = string.match(log_data.date, "(%d%d)/(%d%d)/(%d%d)")
+        -- formatted_date = string.format("20%s-%s-%s", year, month, day)
 
-        record["log_date"] = formatted_date
-        record["log_time"] = log_data.time
+        log_time = log_data.time
         record["level_info"] = log_data.level
-        record["log_datetime"] = formatted_date .. " " .. log_data.time
-        record["message"] = log_data.message
+        record["message"] = log_message --log_data.message
         
         record["log"] = nil
 
         return 1, timestamp, record
     else
-        return -1, timestamp, record
+        record["message"] = log_message
+        record["log"] = nil
+
+        -- log_level 설정
+        if string.match(log_message, "Traceback") then
+            record["level_info"] = "ERROR"
+            return 1, timestamp, record
+        else
+            record["level_info"] = "ETC"
+            return -1, timestamp, record
+        end
     end
 end
