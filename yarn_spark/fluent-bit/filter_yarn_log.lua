787208acbeb4815193f89b9a6be13116d6da580e
barnacles.lee <barnacles.lee@kakaohealthcare.com>
2025-05-19 11:06:42 +0900

    spark file log 추가

--- yarn_spark/fluent-bit/filter_yarn_log.lua
+++ yarn_spark/fluent-bit/filter_yarn_log.lua
@@ -57,3 +57,47 @@ function filter_khc_yarn_log(tag, timestamp, record)
         end
     end
 end
+
+function filter_khc_spark_application_info(tag, timestmap, record)
+    -- 권한(예: lrwxrwxrwx)과 파일명 캡처
+    -- lrwxrwxrwx 1 ubuntu ubuntu   75 May 16 05:48 llm_mart_transfer_data.py -> /hadoop/yarn/local/usercache/airflow/filecache/13/llm_mart_transfer_data.py
+
+    local path = record["file_path"]
+    local applicationid = string.match(path, "application_%d+_%d+")
+    
+    if applicationid then
+        record["application_id"] = applicationid
+    end
+
+    local log_message = record["log"]
+    local log_pattern = "([%w_]+%.py)%s+->"
+    local file_name = string.match(log_message, log_pattern)
+
+    -- Get current date and time
+    local current_date = os.date("%Y-%m-%d")
+    local current_time = os.date("%H:%M:%S")
+    local current_datetime = current_date .. " " .. current_time
+    
+    record["log_datetime"] = current_datetime   
+    record["log_date"] = current_date
+    record["log_time"] = current_time
+
+    record["date"] = timestmap  --timestmap
+
+    if file_name then
+        record["file_name"] = file_name
+        record["log"] = nil
+        record["message"] = log_message
+
+        local sperator_pattern = "^(%a+)_([%w_]+)%.py$"
+        local load_type, table_name = string.match(file_name, sperator_pattern)
+        
+        record["load_type"] = load_type
+        record["table_name"] = table_name
+
+        return 1, record.date, record
+    else
+        return -1, timestamp, record
+    end
+    
+end
